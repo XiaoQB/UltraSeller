1ee789209175c8b21dc96d395d9598bbc9a3b7c4
ningzhong.wyl <ningzhong.wyl@antfin.com>
2020-10-15 11:40:28 +0800

    NAS增加日期

--- src/main/java/com/alibaba/sca/temp/web/NasFileController.java
+++ src/main/java/com/alibaba/sca/temp/web/NasFileController.java
@@ -4,6 +4,9 @@ import java.io.File;
 import java.io.PrintWriter;
 import java.io.RandomAccessFile;
 import java.io.StringWriter;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -34,20 +37,22 @@ public class NasFileController {
 		try {
 			File filePath = new File( nasPath );
 			if (filePath.exists()) {
-				sb.append( "reWriteFile NAS本地路径: " + nasPath ).append("，存在！").append("<br>");
+				sb.append( "writeFile NAS本地路径: " + nasPath ).append("，存在！").append("<br>");
 				// 创建文件
 				String newFileName = filePath.getPath() + "/abc.txt";
 				// 打开一个随机访问文件流，按读写方式   
-	            RandomAccessFile randomFile = new RandomAccessFile( newFileName, "rw");   
-	            // 文件长度，字节数   
-	            long fileLength = randomFile.length();   
-	            sb.append( "当前文件长度：" +  fileLength ).append("<br>");
-	            // 将写文件指针移到文件尾。   
-	            randomFile.seek(fileLength);   
-	            randomFile.write( "SpringBoot服务写入！".getBytes("UTF-8") );   
-	            randomFile.close();   
+        RandomAccessFile randomFile = new RandomAccessFile( newFileName, "rw");   
+        // 文件长度，字节数   
+        long fileLength = randomFile.length();   
+        sb.append( "当前文件长度：" +  fileLength ).append("<br>");
+        // 将写文件指针移到文件尾。   
+        randomFile.seek(fileLength);   
+        DateFormat df = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        String strTime = df.format( Calendar.getInstance().getTime() );
+        randomFile.write( (strTime + "SpringBoot服务写入！").getBytes("UTF-8") );   
+        randomFile.close();   
 			} else {
-				sb.append( "NAS本地路径: 不存在" ).append("<br>");
+				sb.append( "NAS本地路径: " + nasPath + "不存在" ).append("<br>");
 			}
 		} catch (Exception e) {
 			sb.append( "发生异常：" + ex2String(e) ).append("<br>");
@@ -55,18 +60,18 @@ public class NasFileController {
 	}
 	
 	private void readFile( StringBuilder  sb ) {
-		sb.append( "readFile 读文件内容: " + nasPath ).append("<br>");
+		sb.append( "readFile 读文件内容: " + nasPath + "/abc.txt" ).append("<br>");
 		try {
-			File filePath = new File( nasPath );
+      File filePath = new File( nasPath );
 			// 创建文件
-			String newFileName = filePath.getPath() + "/abc.txt";
+      String newFileName = filePath.getPath() + "/abc.txt";
 			// 打开一个随机访问文件流，按读写方式   
-	        RandomAccessFile randomFile = new RandomAccessFile( newFileName, "rw");   
-	        long fileLength = randomFile.length();   
-	        while( randomFile.getFilePointer()<fileLength ) {
-	        	sb.append( new String( randomFile.readLine().getBytes("ISO8859_1"), "UTF-8" )).append("<br>");
-	        }
-	        randomFile.close();   
+      RandomAccessFile randomFile = new RandomAccessFile( newFileName, "rw");   
+      long fileLength = randomFile.length();   
+      while( randomFile.getFilePointer()<fileLength ) {
+        sb.append( new String( randomFile.readLine().getBytes("ISO8859_1"), "UTF-8" )).append("<br>");
+      }
+      randomFile.close();   
 		} catch (Exception e) {
 			sb.append( "读文件发生异常：" + ex2String(e) ).append("<br>");
 		}
