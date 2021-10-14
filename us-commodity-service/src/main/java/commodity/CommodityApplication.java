8ee73736cf2c07c64c173f15c0cab5d113287d4c
shuleiKuang <923950043@qq.com>
2021-10-14 13:40:56 +0800

    Init Project

---
+++ us-commodity-service/src/main/java/commodity/CommodityApplication.java
@@ -0,0 +1,11 @@
+package commodity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommodityApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(CommodityApplication.class, args);
+    }
+}
