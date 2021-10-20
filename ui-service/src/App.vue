49078c3f30f9173396cf9dcf14e7aaad2c1a7d51
peichengXie <xiepeich123@163.com>
2021-10-20 14:03:48 +0800

    登陆注册界面

--- ui-service/src/App.vue
+++ ui-service/src/App.vue
@@ -1,17 +1,19 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <router-view></router-view>
   </div>
 </template>
-
+<script src = "main.js"></script>
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
+
+//mport HelloWorld from './components/HelloWorld.vue'
+import login from './components/login.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    // eslint-disable-next-line vue/no-unused-components
   }
 }
 </script>
