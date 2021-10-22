a473b3f1b2d3925030da8a045b515d7ffe751a16
pengjiahan <1250308041@qq.com>
2021-10-22 15:56:16 +0800

    fix import

--- us-user-service/src/main/java/user/controller/UserController.java
+++ us-user-service/src/main/java/user/controller/UserController.java
@@ -1,7 +1,7 @@
 package user.controller;
 
 import lombok.extern.slf4j.Slf4j;
-import org.apache.commons.lang.StringUtils;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import user.domain.dto.GetUserListDTO;
