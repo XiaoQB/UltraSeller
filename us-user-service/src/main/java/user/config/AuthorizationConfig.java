80535a2ca227595a1fcf98c93754b6d85ffb5f79
caoyufei <slhcyf2015@163.com>
2021-10-23 16:12:01 +0800

    fix:bug01

--- us-user-service/src/main/java/user/config/AuthorizationConfig.java
+++ us-user-service/src/main/java/user/config/AuthorizationConfig.java
@@ -18,6 +18,7 @@ public class AuthorizationConfig implements WebMvcConfigurer {
         registration.addPathPatterns("/**");                      //所有路径都被拦截
         registration.excludePathPatterns(                         //添加不拦截路径
                 "/user/login",            //登录
+                "/user/register",            //登录
                 "/**/*.js",              //js静态资源
                 "/**/*.css",             //css静态资源
                 "/**/*.woff",
