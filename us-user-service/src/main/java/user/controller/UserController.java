c3d69eb5c9be780a68c06e54cd18a799002e02d3
Keyon-2580 <xiepeich123@163.com>
2021-10-24 17:37:54 +0800

    fix: match frontend

--- us-user-service/src/main/java/user/controller/UserController.java
+++ us-user-service/src/main/java/user/controller/UserController.java
@@ -40,10 +40,10 @@ public class UserController {
         return new R<>(ResultCode.SUCCESS.getCode(), ResultCode.SUCCESS.getMessage(), jwt);
     }
 
-    @GetMapping("/user/info/{role}/{num}/{page}")
-    public R<GetUserListDTO> getUserList(@PathVariable("role") String role,
-                                     @PathVariable("num") Integer num,
-                                     @PathVariable("page") Integer page){
+    @GetMapping("/user/info")
+    public R<GetUserListDTO> getUserList( String role,
+                                      Integer num,
+                                     Integer page){
         System.out.println(role+num+page);
         if(num==null||page==null){
             log.info("info 查询数值错误");
