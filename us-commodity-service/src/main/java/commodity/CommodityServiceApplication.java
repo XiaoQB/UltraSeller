fed4ed75d000f379963d37a7d1f5a3170d1279dd
zengpengwu <821776149@qq.com>
2021-10-31 23:37:20 +0800

    add: eureka service, eureka client test on commodity

--- us-commodity-service/src/main/java/commodity/CommodityServiceApplication.java
+++ us-commodity-service/src/main/java/commodity/CommodityServiceApplication.java
@@ -4,10 +4,12 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 //@MapperScan(basePackages = {"commodity.mapper"})
+@EnableEurekaClient
 public class CommodityServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(CommodityServiceApplication.class, args);
