a1c20e264c659b4a6ab2ac9eccbd39afd72be91b
Keyon-2580 <xiepeich123@163.com>
2021-11-17 10:55:46 +0800

    feat :图片上传功能

--- ui-service/src/config/index.js
+++ ui-service/src/config/index.js
@@ -2,8 +2,8 @@ module.exports = {
     devServer: {
         proxy: {
             '/api': {
-                // 此处的写法，目的是为了 将 /api 替换成 https://www.baidu.com/
-                target: 'https://127.0.0.1:8002',
+                // 此处的写法，目的是为了 将 /api 替换成 目标ip
+                target: 'https://127.0.0.1:8000',
                 // 允许跨域
                 changeOrigin: true,
                 ws: true,
