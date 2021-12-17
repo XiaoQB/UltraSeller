9865c31f1114ca490b3b6460caac60e213022d83
QuanbinXiao <plus2019@foxmail.com>
2021-12-17 17:09:45 +0800

    k8s

--- kubernetes/k8s_apply.sh
+++ kubernetes/k8s_apply.sh
@@ -1,10 +1,7 @@
-# kubectl apply -f networkpolicy.yaml
-kubectl apply -f user-db.yaml
-kubectl apply -f cart-db.yaml
-kubectl apply -f eureka.yaml
-kubectl apply -f gateway.yaml
+kubectl apply -f commodity.yaml
 kubectl apply -f user.yaml
-kubectl apply -f cart.yaml
+#kubectl apply -f gateway.yaml
+#kubectl apply -f cart.yaml
 
-kubectl autoscale deployment user-service -n ase-ns-11 --cpu-percent=50 --min=1 --max=10
-kubectl autoscale deployment shopping-cart-service -n ase-ns-11 --cpu-percent=50 --min=1 --max=10
+#kubectl autoscale deployment user-service -n ase-ns-11 --cpu-percent=50 --min=1 --max=10
+#kubectl autoscale deployment shopping-cart-service -n ase-ns-11 --cpu-percent=50 --min=1 --max=10
