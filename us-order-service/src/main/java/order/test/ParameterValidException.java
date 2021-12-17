39be7181e22eb0d6039e3fc71533f25d2110591c
caoyufei <slhcyf2015@163.com>
2021-12-17 14:28:23 +0800

    bug

---
+++ us-order-service/src/main/java/order/test/ParameterValidException.java
@@ -0,0 +1,13 @@
+package order.test;
+
+/**
+ * description:
+ *
+ * @author caoyufei
+ * create:
+ **/
+public class ParameterValidException extends Exception{
+    public ParameterValidException(String msg){
+        super(msg);
+    }
+}
