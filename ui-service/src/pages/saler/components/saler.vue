7fabdc7efb96fccba66cd5676a90233df1dbb343
Keyon-2580 <xiepeich123@163.com>
2021-12-18 20:08:29 +0800

    fix ：买家卖家信息不通

--- ui-service/src/pages/saler/components/saler.vue
+++ ui-service/src/pages/saler/components/saler.vue
@@ -514,6 +514,11 @@ export default {
   width: 100%;
   height: 100%;
   margin-top: 50px;
+  display: grid;
+  /*  声明列的宽度  */
+  grid-template-columns: repeat(5, 20%);
+  /*  声明行的高度  */
+  grid-template-rows: 550px;
 }
 
 .item-image {
