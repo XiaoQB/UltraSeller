4f5d0969bd9fe88e048a4bc5196f6c305929238d
QuanbinXiao <plus2019@foxmail.com>
2021-12-18 14:34:27 +0800

    redeploy new version

--- kubernetes/prepare_docker_images.bat
+++ kubernetes/prepare_docker_images.bat
@@ -1,6 +1,8 @@
 mvn clean package
-echo "login  with password" & docker login --username=75560 --password 75560zcb
-echo "build docker images and tag" & docker build -t ultraseller-users:latest ./us-user-service
+echo "login  with password" &
+docker login --username=75560 --password 75560zcb
+echo "build docker images and tag" &
+docker build -t ultraseller-users:latest ./us-user-service
 docker build -t 75560/ultraseller-commodity:latest ./us-commodity-service
 docker build -t 75560/ultraseller-gateway:latest ./us-gateway-service
 docker build -t 75560/ultraseller-wallet:latest ./us-wallet-service
